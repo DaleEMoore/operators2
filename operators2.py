8e135f92a4c3a582f1b84a472e3375f1e302a98e
Bill Spinks <bill.spinks@gmail.com>
2014-08-27 10:35:45 -0500

    Initial commit

---
+++ operators2.py
@@ -0,0 +1,22 @@
+__author__ = 'bill'
+
+# this is practice list of operators
+a = "-------------------"
+# ARITHMETIC
+print 1 + 2  # addition
+print 3 -5   #subtraction
+print 6 * 6   #multiplication
+print 6 / 2   #division -- left side divided by riught side
+print 7 % 2   #modulus - divideds leftside from right and returns remainder
+print a
+#COMPARISON
+print 3 == 3  #equal of not
+print 3 == 1   #equal or not
+print 3 != 3  #equal of not  --- I do not understand difference here between
+print 3 != 1   #equal or not     the == and != operators
+print a
+print 6 > 3
+print 3 < 6
+print 3 <= 6
+print 6 <= 3
+print 7 < 10
